ddfeeae3559ef4b096c2aa54cdcb00dc10615256
wxllxvn <willml.contato@gmail.com>
2021-08-31 21:08:54 -0300

    update script

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 window.onload=function(){
 
+
+
+    //Inputs
+
     const bill = document.getElementById('bill');
-    function borderOn(){
+    const people = document.getElementById('people');
+
+    function borderOnInt(){
         var inpBill = document.getElementById('intBillBorder');
-        inpBill.classList.toggle('border-cyan')
-    }
-
-    bill.addEventListener('click', borderOn)
-
-
+        var peopleB = document.getElementById('people-border')
+        inpBill.classList.add('border-cyan')
+        peopleB.classList.remove('border-cyan')
+    };
 
 
+    function borderOffInt(){
+        var inpBill = document.getElementById('intBillBorder');
+        inpBill.classList.remove('border-cyan')
+    };
 
+    function borderOnP(){
+        var peopleB = document.getElementById('people-border');
+        peopleB.classList.add('border-cyan')
+    };
+
+
+    function borderOffP(){
+        var inpBill = document.getElementById('intBillBorder');
+        inpBill.classList.remove('border-cyan')
+    };
+
+
+    function selectTip(){
+        console.log(event.target.id);
+    };
+
+    bill.addEventListener('click', borderOnInt);
+    people.addEventListener('click', borderOffInt)
+
+    people.addEventListener('click', borderOnP)
+
+
+    
 
 
 
